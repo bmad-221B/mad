ec672c7928e00adc9acba6c5fbaddeeca5a874f0
Bhawesh Madhwani <bhawesh.madhwani@gmail.com>
2020-01-09 23:14:46 +0530

    Initial commit

---
+++ app/src/main/java/com/sherlocked/parse_serverstarter/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sherlocked.parse_serverstarter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.parse.ParseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+}
---
+++ app/src/main/java/com/sherlocked/parse_serverstarter/StarterApplication.java
@@ -0,0 +1,55 @@
+package com.sherlocked.parse_serverstarter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //f8VEE282ap7v-Password
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("7122d9ba258535c2ec4d1b929d127d04cf30fad5")
+                .clientKey("c0340ce5695dcb829fa9cbc8d2dafb3b5a42c278")
+                .server("http://13.233.124.193:80/parse/")
+                .build()
+        );
+
+        ParseObject object = new ParseObject("ExampleObject");
+        object.put("myNumber", "123");
+        object.put("myString", "rob");
+
+        object.saveInBackground(new SaveCallback () {
+            @Override
+            public void done(ParseException ex) {
+                if (ex == null) {
+                    Log.i("Parse Result", "Successful!");
+                } else {
+                    Log.i("Parse Result", "Failed" + ex.toString());
+                }
+            }
+        });
+
+
+        ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+}
